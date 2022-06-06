7607e03773b9f4c1bd7d349852016ccf34895181
Vincent Tjia <vtjia51@gmail.com>
2022-06-05 20:41:55 -0400

    wrote models associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Tag, {
+  foreignKey: 'product_id',
+});
 
 module.exports = {
   Product,
